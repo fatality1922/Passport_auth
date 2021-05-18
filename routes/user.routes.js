132869cb572fc07216a742f64d2247b241a83664
Danciak <fatality1922@gmail.com>
2021-05-18 20:01:17 +0200

    add views and authentization

---
+++ routes/user.routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/logged', (req, res) => {
+    req.user ? res.render('logged', {username: req.user.displayName, avatar: req.user.photos[ 0 ].value}) : res.redirect('/user/no-permission');
+});
+
+router.get('/no-permission', (req, res) => {
+  res.render('noPermission');
+});
+
+router.get('/profile', (req, res) => {
+    req.user ? res.render('profile') : res.redirect('/no-permission');
+});
+
+router.get('/profile/settings', (req, res) => {
+    req.user ? res.render('profileSettings') : res.redirect('/no-permission');
+});
+module.exports = router;
--- server.js
+++ server.js
@@ -2,27 +2,33 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const hbs = require('express-handlebars');
+const passport = require('passport');
+const session = require('express-session');
+const passportConfig = require('./config/passport');
+const authRoutes = require('./routes/auth.routes');
+const userRoutes = require('./routes/user.routes');
 
 const app = express();
 
 app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
 
+app.use(session({ secret: 'costam' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '/public')));
 
+app.use('/auth', authRoutes);
+app.use('/user', userRoutes);
+app.use('/userprofile', userRoutes);
+
+
 app.get('/', (req, res) => {
   res.render('index');
-});
-
-app.get('/user/logged', (req, res) => {
-  res.render('logged');
-});
-
-app.get('/user/no-permission', (req, res) => {
-  res.render('noPermission');
 });
 
 app.use('/', (req, res) => {
